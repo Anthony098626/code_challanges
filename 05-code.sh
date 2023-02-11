4d21ac5cc7d753ecc07bcd09249b5596ec4dc3f3
anthonywall <anthony098626@gmail.com>
2023-02-11 07:10:02 +0000

    test

--- 05-code.sh
+++ 05-code.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-# Script:                       
-# Author:                       
-# Date of latest revision:      
-# Purpose: 
+# Script: Class_5                      
+# Author: Anthony Wall                        
+# Date of latest revision: 02/10/2023     
+# Purpose: This scrip displays a running process, asks user for PID, then kills with PID 
+
+# source https://chat.openai.com/chat 
+
+# Main 
+import os
+
+print("List of running processes:")
+processes = os.popen("ps -e").read()
+print(processes)
+
+pid = int(input("Enter PID of the process to be killed: "))
+
+os.system(f"kill {pid}")
+print(f"Process with PID {pid} has been killed.")
+# End 
