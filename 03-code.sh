f74da3e1cec57422979070c298074e5814370e58
anthonywall <anthony098626@gmail.com>
2023-02-11 00:21:37 +0000

    login_history

---
+++ 03-code.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script: 03-code.sh                      
+# Author: Anthony Wall                       
+# Date of latest revision: 02/10/2023     
+# Purpose: This script prints in login history of users on the computer     
+
+# Basic function
+# Main
+
+login_history(){
+    last
+}
+
+login_history
+
+
+# End 
+
+
