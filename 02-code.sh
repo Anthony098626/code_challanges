aa2bfdeacfa841eaa91469d1de83b7a39eac7269
anthonywall <anthony098626@gmail.com>
2023-02-11 00:02:30 +0000

    02-code

--- 02-code.sh
+++ 02-code.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo test 
+
+# Script Name:                  02-code.sh                
+# Class Name:                   Ops 201
+# Author Name:                  Anthony Wall
+# Date of latest revision:      02/07/2023
+# Purpose: creat a script that prints a string to the consol.                      
+
+# Main
+
+echo "this is a string"
+
+
+
+# End
