c5d1db9852167dcefd15331ca958d7d6a254c044
anthonywall <anthony098626@gmail.com>
2023-02-16 04:27:42 +0000

    test

--- 08-code.sh
+++ 08-code.sh
@@ -1,5 +1,13 @@
-# Script Name:                  08-code.sh                
-# Class Name:                   Ops 201
-# Author Name:                  Anthony Wall
-# Date of latest revision:      
-# Purpose: 
+# Script Name:08-code.sh                
+# Class Name:Ops 201
+# Author Name:Anthony Wall
+# Date of latest revision:02/15/2023      
+# Purpose: windows Batch Scripting 
+
+# Main 
+coming back to over the weekend, battaling a cold now.  
+
+
+
+
+# End 
