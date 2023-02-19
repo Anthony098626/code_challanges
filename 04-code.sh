b74b32364b6149623cff58e7afbe9dfc165f883d
anthony098626 <anthony098626@gmail.com>
2023-02-19 00:18:39 +0000

    code4

---
+++ 04-code.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script Name:04-code.sh                
+# Class Name:Ops 201
+# Author Name:Anthony Wall
+# Date of latest revision:02/07/2023
+# Purpose:make directories 
+# source: chatgbt.com used for explanation of new file dir using new var
+
+# Main 
+
+mkdir blue1 green2 yellow3 orange4
+directories=("blue1"'green2""yellow3""orange4")
+
+for dir in "${directories}
+do 
+    touch "${dir}/file.txt"
+
+# End 
