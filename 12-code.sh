3d090865795e873dc0731bf8c0d386f07fc8bf25
anthony098626 <anthony098626@gmail.com>
2023-02-22 06:17:42 +0000

    test

---
+++ 12-code.sh
@@ -0,0 +1,11 @@
+# Script Name:11-code.sh                
+# Class Name:Ops 201
+# Author Name:Anthony Wall
+# Date of latest revision:02/20/2023      
+# Purpose: Automated Endpoint Configuration
+
+#Main 
+
+catching up to  
+
+# End 
