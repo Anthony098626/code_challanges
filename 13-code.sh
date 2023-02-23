582c31ed01804d139573c0302e7a93e6f7066961
anthony098626 <anthony098626@gmail.com>
2023-02-23 03:13:47 +0000

    initial

---
+++ 13-code.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script: Class_13                    
+# Author: Anthony Wall                        
+# Date of latest revision: 02/22/2023     
+# Purpose: This scrip detects is a file or directory exsist and if not creadit it.
+
+cathing up to  
+
