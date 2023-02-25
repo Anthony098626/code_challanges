97612c0ea7d586af2af65f84c41bb98044085cf4
anthony098626 <anthony098626@gmail.com>
2023-02-25 03:57:27 +0000

    resub

--- 11-code.sh
+++ 11-code.sh
@@ -6,6 +6,23 @@
 
 #Main 
 
-catching up to 
+Enable File and Printer Sharing
+Set-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Enabled True
+
+Allow ICMP traffic
+New-NetFirewallRule -DisplayName "Allow ICMPv4-In" -Protocol ICMPv4 -IcmpType 8 -Enabled True
+
+Enable Remote management
+Enable-PSRemoting -Force
+
+Remove bloatware
+Get-MpThreatCatalog | Where-Object {$_.Name -like "Malware Signature"} | Remove-MpThreatCatalogEntry
+
+Enable Hyper-V
+Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart
+
+Disable SMBv1, an insecure protocol
+Set-SmbServerConfiguration -EnableSMB1Protocol $false
+
 
 # End 
