89d36a610c80a7e2bfc0adecb567c406490f2464
anthony098626 <anthony098626@gmail.com>
2023-02-25 03:02:21 +0000

    resub

--- 12-code.sh
+++ 12-code.sh
@@ -1,11 +1,18 @@
-# Script Name:11-code.sh                
+# Script Name:12-code.sh                
 # Class Name:Ops 201
 # Author Name:Anthony Wall
-# Date of latest revision:02/20/2023      
-# Purpose: Automated Endpoint Configuration
+# Date of latest revision:02/24/2023      
+# Purpose: Powershell Script returning IPv4 
 
-#Main 
+# Main 
 
-catching up to  
 
-# End 
+Get-NetIPAddress -AddressFamily IPv4 | Select-Object -ExpandProperty IPAddress
+
+$ipconfig = ipconfig /all
+$ipv4 = $ipconfig | Select-String -Pattern '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}' | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value
+Write-Host "IPv4 address: $ipv4"
+
+#End 
+
+Sourced from chatgbt.com 
