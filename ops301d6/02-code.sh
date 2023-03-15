613c0b7f4c67f59161c63b987aad5c274fa105f4
Anthony <antony098626@gmail.com>
2023-03-14 20:47:52 -0400

    date&time

---
+++ ops301d6/02-code.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script Name:                  03-code.sh                
+# Class Name:                   Ops 301
+# Author Name:                  Anthony Wall
+# Date of latest revision:      03/14/2023
+# Purpose: creat a script that prints prints date and time. 
+
+
+# Get the current date and time
+now=$(date +"%Y-%m-%d_%H-%M-%S")
+
+# Copy the syslog file to the current working directory and append the date and time to the filename
+cp /var/log/syslog ./syslog_$now
+
+#End
+
+# help from chat gbt
