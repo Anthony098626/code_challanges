8229dd991e18236e7f332c5140beab7220f54592
Anthony <antony098626@gmail.com>
2023-03-17 13:45:08 -0400

    readme

---
+++ ops301d6/05-code.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Script Name:                  03-code.sh                
+# Class Name:                   Ops 301
+# Author Name:                  Anthony Wall
+# Date of latest revision:      03/17/2023
+# Purpose:
