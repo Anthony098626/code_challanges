bab2274fb269b455a75a4465648fb2b507480b7b
Anthony <antony098626@gmail.com>
2023-03-22 23:30:37 -0400

    class8

---
+++ ops301d6/06-code.py
@@ -0,0 +1,7 @@
+#!/bin/env python
+
+# Script Name:                  03-code.sh                
+# Class Name:                   Ops 301
+# Author Name:                  Anthony Wall
+# Date of latest revision:      03/20/2023
+# Purpose: Creats a script to perfrom a python script.
---
+++ ops301d6/08-code.py
@@ -0,0 +1,23 @@
+#!/bin/env python
+
+# Script Name:                  03-code.sh                
+# Class Name:                   Ops 301
+# Author Name:                  Anthony Wall
+# Date of latest revision:      03/22/2023
+# Purpose: Creats a python script. 
+
+# Assign a list of ten string elements to a variable
+my_list = ["Smith", "Glock", "Colt", "Remington", "Ruger", "Sig", "Baretta", "Winchester", "Browning", "Savage"]
+
+# Print the fourth element of the list
+print("Fourth element of the list:", my_list[3])
+
+# Print the sixth through tenth element of the list
+print("Sixth through tenth elements of the list:", my_list[5:10])
+
+# Change the value of the seventh element to "onion"
+my_list[6] = "onion"
+print("List after changing the value of the seventh element:", my_list)
+
+source
+* chatgbt.com
